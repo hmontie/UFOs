3c1cc2541d3806c49453eafcc15403f3e6214842
Haley Monteblanco <haley.anne.stewart@gmail.com>
2022-09-07 19:47:53 -0700

    update

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// First, clear out any existing data
+function buildTable(data) {
+    tbody.html("");
+}
+
+// Next, loop through each object in the data
+// and append a row and cells for each value in the row
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+});
+
+// Loop through each field in the dataRow and add
+// each value as a table cell (td)
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      // appending data into a table data tag
+      cell.text(val);
+      }
+    );
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
